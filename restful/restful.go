18eb5ea224968625a950749b206e54815ab54559
Sergey Romanov <xxsmotur@gmail.com>
2016-04-04 23:49:49 +0500

    Update restful

--- restful/restful.go
+++ restful/restful.go
@@ -1,7 +1,9 @@
 package restful
 
 import (
+	"encoding/json"
 	"log"
+	"net/http"
 
 	"github.com/emicklei/go-restful"
 
@@ -22,22 +24,36 @@ func (s *User) UpdateName(req *restful.Request, rsp *restful.Response) {
 	log.Print("Received User.UpdateName API request")
 
 	//name := req.PathParameter("name")
+	email, err := req.BodyParameter("email")
+	if err != nil {
+		rsp.WriteError(500, err)
+	}
+
+	old, err := req.BodyParameter("oldUserName")
+	if err != nil {
+		rsp.WriteError(500, err)
+	}
+
+	newName, err := req.BodyParameter("newUserName")
+	if err != nil {
+		rsp.WriteError(500, err)
+	}
 
 	//var resp account.UpdateNameResponse
 	resp, err := cl.UpdateName(context.TODO(), &account.UpdateNameRequest{
-		Email: "123@mail.ru",
-		Oldusername:"past",
-		Username:"future",
+		Email: email,
+		Oldusername:old,
+		Username:newName,
 	})
 
 	if err != nil {
-		rsp.WriteError(500, err)
+		rsp.WriteError(500,  err)
 	}
 
 	rsp.WriteEntity(resp)
 }
 
-func InitRestful() {
+func InitRestful2() {
 	// Create service
 	service := web.NewService(
 		web.Name("go.micro.srv.user"),
@@ -55,7 +71,8 @@ func InitRestful() {
 	ws.Consumes(restful.MIME_XML, restful.MIME_JSON)
 	ws.Produces(restful.MIME_JSON, restful.MIME_XML)
 	ws.Path("/accounts")
-	ws.Route(ws.POST("/name").To(say.UpdateName))
+	ws.Route(ws.POST("/name").To(say.UpdateName)).Doc("Update name on user").
+	Consumes("application/json")
 	wc.Add(ws)
 
 	// Register Handler
@@ -65,4 +82,43 @@ func InitRestful() {
 	if err := service.Run(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func Update(w http.ResponseWriter, r *http.Request) {
+	var req account.UpdateNameRequest
+	var err error
+	//Trying to decode request body
+	decoder := json.NewDecoder(r.Body)
+	err = decoder.Decode(&req)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	//var resp account.UpdateNameResponse
+	_, err = cl.UpdateName(context.TODO(), &account.UpdateNameRequest{
+		Email: req.Email,
+		Oldusername:req.Oldusername,
+		Username:req.Username,
+	})
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
+func InitRestful() {
+	service := web.NewService(
+		web.Name("go.micro.srv.user"),
+	)
+
+	service.Init()
+
+	// setup Greeter Server Client
+	cl = account.NewAccountClient("go.micro.srv.user", client.DefaultClient)
+	
+	http.HandleFunc("/accounts/name", Update)
+	http.ListenAndServe(":8080", nil)
 }
